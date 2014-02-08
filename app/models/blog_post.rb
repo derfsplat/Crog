0248d5b51ffee268d046b3b871a5ecaca0bea3b5
derfsplat <derfsplat@github>
2014-02-08 12:55:42 -0500

    feat: add comments model

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,5 @@
 class BlogPost < ActiveRecord::Base
   attr_accessible :content, :publishdate, :title, :ispublished
+
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140207015612) do
+ActiveRecord::Schema.define(:version => 20140208165433) do
 
   create_table "blog_posts", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(:version => 20140207015612) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.boolean  "ispublished"
+  end
+
+  create_table "comments", :force => true do |t|
+    t.integer  "blog_post_id"
+    t.string   "email"
+    t.string   "comment"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "users", :force => true do |t|
